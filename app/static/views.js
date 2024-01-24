f22021373cab85d25da2fdb0937eba9ca516c1e4
Rodrigo Azevedo <rodrigoazevedo@Rodrigos-MacBook-Pro.local>
2024-01-24 16:44:12 +0100

    box plot working

--- app/static/views.js
+++ app/static/views.js
@@ -1,87 +1,5 @@
- // Function to update the box plot with user input
- async function updateBoxPlot(bloodIndicator) {
-    // Fetch blood indicator statistics
-    var response = await fetch('/statistics/' + bloodIndicator);
-    var fetchedData = await response.json();
 
-    // Draw the box plot with fetched data and user input
-    drawBoxPlot(fetchedData);
-}
-
-
-
-// Function to draw the box plot
-function drawBoxPlot(data) {
-    // set the dimensions and margins of the graph
-    var margin = { top: 10, right: 40, bottom: 30, left: 10 },
-        width = 400 - margin.left - margin.right,
-        height = 200 - margin.top - margin.bottom;
-
-    // append the svg object to the body of the page
-    var svg = d3.select("#box-plot")
-        .html("")  // Clear previous content
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    // Compute summary statistics used for the box
-    var q1 = data.q1;
-    var median = data.median;
-    var q3 = data.q3;
-    var iqr = data.iqr;
-
-    // Use lower_bound and upper_bound directly
-    var min = data.lower_bound;
-    var max = data.upper_bound;
-
-    // Show the X scale with dynamic domain
-    var x = d3.scaleLinear()
-        .domain([min, max])
-        .range([0, width]);
-    svg.call(d3.axisBottom(x));
-
-    
-    var center = height / 2;
-    var boxHeight = 50;
-
-    // Show the main horizontal line
-    svg.append("line")
-        .attr("class", "main-line")
-        .attr("y1", center)
-        .attr("y2", center)
-        .attr("x1", x(min))
-        .attr("x2", x(max))
-        .attr("stroke", "#333"); // Dark gray
-
-    // Show the box
-    svg.append("rect")
-        .attr("class", "box")
-        .attr("y", center - boxHeight / 2)
-        .attr("x", x(q1))
-        .attr("width", (x(q3) - x(q1)))
-        .attr("height", boxHeight)
-        .attr("stroke", "#333") // Dark gray
-        .style("fill", "#ddd"); // Light gray
-
-    // Show median, min, and max vertical lines
-    svg.selectAll(".lines")
-        .data([min, median, max])
-        .enter()
-        .append("line")
-        .attr("class", "lines")
-        .attr("y1", center - boxHeight / 2)
-        .attr("y2", center + boxHeight / 2)
-        .attr("x1", function (d) { return x(d); })
-        .attr("x2", function (d) { return x(d); })
-        .attr("stroke", "#666"); // Dark gray
-}
-
-
-
-
-let input = document.getElementById('patientIDPatientInfo');
+const input = document.getElementById('patientIDPatientInfo');
 input.addEventListener('input', async function () {
     // Get values from form inputs
     var patientID = input.value;
@@ -179,24 +97,146 @@ function displayPatientInfo(data) {
     }
 }
 
+function drawHeatmap(patientID, date) {
+
+    // Remove existing SVG element
+    d3.select('#heatmap').selectAll('*').remove();
+    d3.select('#date').remove();
+    fetch(`/blood_tests/raw/${patientID}?date=${date}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+            
+        })
+        .then(data => {
+            
+        const combined_dict = data; 
+        // Convert data to suitable format
+        const dataArray = [];
+        const dict = data; // Assuming there's only one dictionary in the list
+
+        // Iterate over the dictionary entries
+        for (const [key, [value, color]] of Object.entries(dict)) {
+            dataArray.push({ key, value, color });
+        }
+
+    
+        // Define margin and cell size
+        const margin = { top: 10, right: 0, bottom: 20, left: 10};
+        const cellSize = 30;
+        const cellWidth = 250;
+        const cellHeight = 25;
+
+        d3.select('#date').text(`Blood test: ${date}`);
+        // Create SVG element
+        const svg = d3.select('#heatmap').append('svg')
+            .attr('width', 600) // Increased width to accommodate values on the right
+            .attr('height', dataArray.length * cellHeight + margin.top + margin.bottom)
+            .style('margin', margin.top + 'px ' + margin.right + 'px ' + margin.bottom + 'px ' + margin.left + 'px');
+
+        // Define color scale based on color strings
+        const colorScale = d3.scaleOrdinal()
+            .domain(['green', 'yellow', 'red'])
+            .range(['green', '#ffd800', '#c80815']);
+
+        // Draw cells
+        svg.selectAll('rect')
+            .data(dataArray)
+            .enter().append('rect')
+            .attr('x', margin.left)
+            .attr('y', (d, i) => margin.top + i * cellHeight)
+            .attr('width', cellWidth)
+            .attr('height', cellHeight)
+            .style('fill', d => colorScale(d.color))
+            .on('mouseover', function (event, d) {
+                // Display blood level on mouse hover
+                d3.select(this).append('title').text(`Blood Level: ${d.value}`);
+            })
+            .on('click', function (event, d) {
+                console.log(`Clicked on text with key: ${d.key} ${d.value}`);
+                heatmap_cell_clicked(d.key, d.value);
+            });
+
+        // Add y-axis labels with word wrapping
+        svg.selectAll('text')
+            .data(dataArray)
+            .enter().append('text')
+            .attr('x', margin.left + cellWidth / 2) // Place text in the center of the cell
+            .attr('y', (d, i) => margin.top + (i + 0.5) * cellHeight) // Use the center of each cell
+            .attr('dy', '0.10em')
+            .style('text-anchor', 'middle') // Center the text horizontally
+            .style('alignment-baseline', 'middle')
+            .style('fill', 'black')
+            .text(function (d) {
+                // Extract and display only the relevant part of the key
+                const words = d.key.split(/\s+/);
+                const keyWithoutUnits = words.slice(0, -1).join(' '); // Exclude the last word (metric unit)
+                return keyWithoutUnits;
+            })
+            .on('click', function (event, d) {
+                // Call your function here with the desired parameters
+                // For example, you can replace the alert with your function call
+                console.log(`Clicked on text with key: ${d.key} ${d.value}`);
+                heatmap_cell_clicked(d.key, d.value);
+            });
+
+        // Draw black lines between cells
+        svg.selectAll('line')
+            .data(dataArray)
+            .enter().append('line')
+            .attr('x1', margin.left)
+            .attr('x2', margin.left + cellWidth)
+            .attr('y1', (d, i) => margin.top + (i + 1) * cellHeight)
+            .attr('y2', (d, i) => margin.top + (i + 1) * cellHeight)
+            .style('stroke', 'black');
+
+        
+        // Add text elements to display values and metric units on the right side of the heatmap
+        svg.selectAll('.value-text')
+        .data(dataArray)
+        .enter().append('text')
+        .attr('class', 'value-text')
+        .attr('x', margin.left + cellWidth + 10)
+        .attr('y', (d, i) => margin.top + i * cellHeight + cellHeight / 2)
+        .attr('dy', '0.20em')
+        .style('fill', 'black')
+        .text(d => `${d.value} ${d.key.split(/\s+/).pop()}`)
+        .on('click', function (event, d) {
+            console.log(`Clicked on text with key: ${d.key} ${d.value}`);
+            heatmap_cell_clicked(d.key, d.value);
+        }); 
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+        // Add error handling here, e.g., display an error message to the user
+    });
+}
 
 
-let dropdown = document.getElementById('bloodIndicators');
-dropdown.addEventListener('change', async function () {
+
+async function heatmap_cell_clicked(blood_indicator, blood_indicator_value) {
     try {
         let references = await fetch('/blood_tests_references/raw/' + input.value);
         let ranges = await references.json();
         let response = await fetch('/all_blood_tests/raw/' + input.value);
         let blood_tests = await response.json();
-        getValuesOverTime(blood_tests, dropdown.value, ranges);
+        getValuesOverTime(blood_tests, blood_indicator, ranges, blood_indicator_value);
     } catch (error) {
         console.error('Error fetching or parsing data:', error);
     }
-});
+}
+
 
 let boxPlotFormCreated = false; // Use a flag to track if the form is created
 
-function getValuesOverTime(formattedBloodTests, selectedIndicator, ranges) {
+function getValuesOverTime(formattedBloodTests, selectedIndicator, ranges, blood_indicator_value) {
     // Filter blood tests for the selected indicator
     const filteredTests = formattedBloodTests.filter(test => selectedIndicator in test);
 
@@ -216,10 +256,6 @@ function getValuesOverTime(formattedBloodTests, selectedIndicator, ranges) {
     });
     const range = ranges[selectedIndicator]
    
-    // Display results on the screen
-    const resultContainer = document.getElementById('resultsContainer');
-    //resultContainer.innerHTML = '<h4>Results overtime for ' + selectedIndicator + '</h4>';
-
     display_history_mean(range, blood_indicator_history, selectedIndicator)
     generateHistogram(blood_indicator_history,selectedIndicator);
     
@@ -242,7 +278,7 @@ function getValuesOverTime(formattedBloodTests, selectedIndicator, ranges) {
         violinPlotDiv.appendChild(demographicDistributionDiv);
         boxPlotFormCreated = true; // Set the flag to true after creating the form
     } 
-    updateBoxPlot(selectedIndicator.split(" ").slice(0, -1).join(' ').trim())
+    updateBoxPlot(selectedIndicator.split(" ").slice(0, -1).join(' ').trim(), blood_indicator_value);
    
 }
 
@@ -345,118 +381,94 @@ svg.selectAll(".bar-label")
     .text(entry => entry.Value[0]);
 }
 
-function drawHeatmap(patientID, date) {
-
-    // Remove existing SVG element
-    d3.select('#heatmap').selectAll('*').remove();
-    d3.select('#date').remove();
-    fetch(`/blood_tests/raw/${patientID}?date=${date}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-            
-        })
-        .then(data => {
-            
-        const combined_dict = data; 
-        // Convert data to suitable format
-        const dataArray = [];
-        const dict = data; // Assuming there's only one dictionary in the list
-
-        // Iterate over the dictionary entries
-        for (const [key, [value, color]] of Object.entries(dict)) {
-            dataArray.push({ key, value, color });
-        }
-
-    
-        // Define margin and cell size
-        const margin = { top: 10, right: 0, bottom: 20, left: 10};
-        const cellSize = 30;
-        const cellWidth = 250;
-        const cellHeight = 25;
-
-        d3.select('#date').text(`Blood test: ${date}`);
-        // Create SVG element
-        const svg = d3.select('#heatmap').append('svg')
-            .attr('width', 600) // Increased width to accommodate values on the right
-            .attr('height', dataArray.length * cellHeight + margin.top + margin.bottom)
-            .style('margin', margin.top + 'px ' + margin.right + 'px ' + margin.bottom + 'px ' + margin.left + 'px');
-
-        // Define color scale based on color strings
-        const colorScale = d3.scaleOrdinal()
-            .domain(['green', 'yellow', 'red'])
-            .range(['green', '#ffd800', '#c80815']);
-
-        // Draw cells
-        svg.selectAll('rect')
-            .data(dataArray)
-            .enter().append('rect')
-            .attr('x', margin.left)
-            .attr('y', (d, i) => margin.top + i * cellHeight)
-            .attr('width', cellWidth)
-            .attr('height', cellHeight)
-            .style('fill', d => colorScale(d.color))
-            .on('mouseover', function (event, d) {
-                // Display blood level on mouse hover
-                d3.select(this).append('title').text(`Blood Level: ${d.value}`);
-            });
-
-        // Add y-axis labels with word wrapping
-        svg.selectAll('text')
-            .data(dataArray)
-            .enter().append('text')
-            .attr('x', margin.left + cellWidth / 2) // Place text in the center of the cell
-            .attr('y', (d, i) => margin.top + (i + 0.5) * cellHeight) // Use the center of each cell
-            .attr('dy', '0.10em')
-            .style('text-anchor', 'middle') // Center the text horizontally
-            .style('alignment-baseline', 'middle')
-            .style('fill', 'black')
-            .text(function (d) {
-                // Extract and display only the relevant part of the key
-                const words = d.key.split(/\s+/);
-                const keyWithoutUnits = words.slice(0, -1).join(' '); // Exclude the last word (metric unit)
-                return keyWithoutUnits;
-            });
 
 
+ // Function to update the box plot with user input
+ async function updateBoxPlot(bloodIndicator, value) {
+    // Fetch blood indicator statistics
+    var response = await fetch('/statistics/' + bloodIndicator);
+    var fetchedData = await response.json();
+    console.log(fetchedData, value);
 
-        // Draw black lines between cells
-        svg.selectAll('line')
-            .data(dataArray)
-            .enter().append('line')
-            .attr('x1', margin.left)
-            .attr('x2', margin.left + cellWidth)
-            .attr('y1', (d, i) => margin.top + (i + 1) * cellHeight)
-            .attr('y2', (d, i) => margin.top + (i + 1) * cellHeight)
-            .style('stroke', 'black');
-
-        
-        // Add text elements to display values and metric units on the right side of the heatmap
-        svg.selectAll('.value-text')
-        .data(dataArray)
-        .enter().append('text')
-        .attr('class', 'value-text')
-        .attr('x', margin.left + cellWidth + 10)
-        .attr('y', (d, i) => margin.top + i * cellHeight + cellHeight / 2)
-        .attr('dy', '0.20em')
-        .style('fill', 'black')
-        .text(d => `${d.value} ${d.key.split(/\s+/).pop()}`); 
-
-        
-
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-        // Add error handling here, e.g., display an error message to the user
-    });
+    // Draw the box plot with fetched data and user input
+    drawBoxPlot(fetchedData, value);
 }
 
 
+// Function to draw the box plot
+function drawBoxPlot(data, value) {
+    // set the dimensions and margins of the graph
+    var margin = { top: 10, right: 40, bottom: 30, left: 10 },
+        width = 400 - margin.left - margin.right,
+        height = 200 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#box-plot")
+        .html("")  // Clear previous content
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Compute summary statistics used for the box
+    var q1 = data.q1;
+    var median = data.median;
+    var q3 = data.q3;
+    var iqr = data.iqr;
+
+    // Use lower_bound and upper_bound directly
+    var min = data.lower_bound;
+    var max = data.upper_bound;
+
+    // Show the X scale with dynamic domain
+    var x = d3.scaleLinear()
+        .domain([min, max])
+        .range([0, width]);
+    svg.call(d3.axisBottom(x));
+
+    // a few features for the box
+    var center = height / 2;
+    var boxHeight = 50;
+
+    // Show the main horizontal line
+    svg.append("line")
+        .attr("class", "main-line")
+        .attr("y1", center)
+        .attr("y2", center)
+        .attr("x1", x(min))
+        .attr("x2", x(max))
+        .attr("stroke", "#333"); // Dark gray
+
+    // Show the box
+    svg.append("rect")
+        .attr("class", "box")
+        .attr("y", center - boxHeight / 2)
+        .attr("x", x(q1))
+        .attr("width", (x(q3) - x(q1)))
+        .attr("height", boxHeight)
+        .attr("stroke", "#333") 
+        .style("fill", "#ddd"); 
+
+    // Show median, min, max, and user input vertical lines
+    svg.selectAll(".lines")
+        .data([min, median, max, value])
+        .enter()
+        .append("line")
+        .attr("class", "lines")
+        .attr("y1", center - boxHeight / 2)
+        .attr("y2", center + boxHeight / 2)
+        .attr("x1", function (d) { return x(d); })
+        .attr("x2", function (d) { return x(d); })
+        .attr("stroke", function (d) { return (d === value) ? "red" : "#666"; }) 
+        .attr("stroke-width", function (d) { return (d === value) ? 2 : 1; }); 
+
+    // Add label for the red line
+    svg.append("text")
+        .attr("x", width - 10)  // Adjust the position as needed
+        .attr("y", center - boxHeight / 2 - 10)  // Adjust the position as needed
+        .attr("text-anchor", "end")
+        .style("fill", "red")
+        .text("Patient Blood Value");
+}
 
